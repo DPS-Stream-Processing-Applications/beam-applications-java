01ec617dc1ee73c2e79b88af474438e39a067f42
eprader <56026248+eprader@users.noreply.github.com>
2024-02-19 22:24:08 +0100

    add devenv flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Bsc project on improved scheduling / resource management for stream processing";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            flink
+            jdk11
+            gradle
+            maven
+          ];
+
+          /* INFO:
+           * The flink package from the nix store contains the helper scripts
+           * inside the `opt` folder. To make these scripts available the folder
+           * gets addet do `PATH`.
+           */
+          shellHook = ''
+            export PATH=$PATH:${pkgs.flink}/opt/flink/bin
+          '';
+        };
+      };
+    };
+}

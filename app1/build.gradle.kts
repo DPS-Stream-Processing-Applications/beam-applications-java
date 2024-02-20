ff67137371b36ee8686d48a5f6a7c2d288199a38
eprader <56026248+eprader@users.noreply.github.com>
2024-02-20 16:17:18 +0100

    add spotless

--- app1/build.gradle.kts
+++ app1/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("application")
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 repositories {
@@ -22,6 +23,30 @@ application {
     mainClass = "com.example.App"
 }
 
+spotless {
+
+    format( "misc", {
+      // define the files to apply `misc` to
+      target("*.gradle", ".gitattributes", ".gitignore")
+
+      // define the steps to apply to those files
+      trimTrailingWhitespace()
+      indentWithTabs() // or spaces. Takes an integer argument if you don't like 4
+      endWithNewline()
+    })
+
+    kotlinGradle {
+        ktlint()
+    }
+
+    java {
+        removeUnusedImports()
+        importOrder()
+        googleJavaFormat("1.8").aosp().reflowLongStrings()
+        formatAnnotations()
+    }
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
@@ -36,7 +61,7 @@ tasks.named<Jar>("jar") {
     destinationDirectory.set(file("build"))
     manifest {
         attributes(
-            "Main-Class" to "com.example.App"
+            "Main-Class" to "com.example.App",
         )
     }
     exclude("META-INF/*.SF")

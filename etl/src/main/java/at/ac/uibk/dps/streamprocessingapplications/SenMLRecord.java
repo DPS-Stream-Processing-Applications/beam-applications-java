5fc4b5b14c7b45238ce491ba054ed96db664d5e8
eprader <56026248+eprader@users.noreply.github.com>
2024-02-22 00:23:00 +0100

    formatting

--- etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/SenMLRecord.java
+++ etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/SenMLRecord.java
@@ -1,22 +1,24 @@
 package at.ac.uibk.dps.streamprocessingapplications;
 
-import org.json.JSONObject;
-import org.json.JSONString;
-
 import java.time.Instant;
+import org.apache.beam.sdk.coders.DefaultCoder;
+import org.apache.beam.sdk.extensions.avro.coders.AvroCoder;
+import org.json.JSONObject;
 
 /**
- * This class represents a SenML sensor value formed of a subset of the official specification.
- * See: https://datatracker.ietf.org/doc/html/rfc8428
- * The following SenML labels are required for this representation:
+ * This class represents a SenML sensor value formed of a subset of the official specification. See:
+ * https://datatracker.ietf.org/doc/html/rfc8428 The following SenML labels are required for this
+ * representation:
+ *
  * <ul>
- *     <li>bn</li>
- *     <li>n</li>
- *     <li>u</li>
- *     <li>v</li>
- *     <li>t</li>
+ *   <li>bn
+ *   <li>n
+ *   <li>u
+ *   <li>v
+ *   <li>t
  * </ul>
  */
+@DefaultCoder(AvroCoder.class)
 public class SenMLRecord {
     private String baseName;
 
@@ -31,7 +33,10 @@ public class SenMLRecord {
         this.name = record.has("n") ? record.optString("n") : null;
         this.unit = record.has("u") ? record.optString("u") : null;
         this.value = record.has("v") ? Float.valueOf(record.optString("v")) : null;
-        this.time = record.has("t") ? Instant.ofEpochSecond(Long.parseLong(record.optString("t")))  : null;
+        this.time =
+                record.has("t")
+                        ? Instant.ofEpochSecond(Long.parseLong(record.optString("t")))
+                        : null;
     }
 
     public String getBaseName() {
@@ -53,6 +58,4 @@ public class SenMLRecord {
     public Instant getTime() {
         return time;
     }
-
 }
-

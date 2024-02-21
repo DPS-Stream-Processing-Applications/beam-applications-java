cd5596642d55ea2a04cd6d85621dd5a0fb75385d
eprader <56026248+eprader@users.noreply.github.com>
2024-02-21 23:08:03 +0100

    update testing dependencies

--- buildSrc/src/main/kotlin/flink-job-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/flink-job-conventions.gradle.kts
@@ -12,7 +12,8 @@ dependencies {
     implementation("org.apache.beam:beam-runners-flink-1.16:2.54.0")
     implementation("org.slf4j:slf4j-jdk14:1.7.32")
 
-    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.hamcrest:hamcrest:2.2")
 }
 

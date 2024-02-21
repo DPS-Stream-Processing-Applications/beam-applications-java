1084e5745662abcfe29e6e451fe5715c45ca6cac
eprader <56026248+eprader@users.noreply.github.com>
2024-02-22 00:24:52 +0100

    implement initial pipeline with SenMLRecord

--- etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/EtlJob.java
+++ etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/EtlJob.java
@@ -1,40 +1,90 @@
 package at.ac.uibk.dps.streamprocessingapplications;
 
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import org.apache.beam.runners.flink.FlinkPipelineOptions;
 import org.apache.beam.runners.flink.FlinkRunner;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.GenerateSequence;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.Filter;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptor;
 import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class EtlJob {
 
     public static void main(String[] args) {
-        var options =
+        FlinkPipelineOptions options =
                 PipelineOptionsFactory.fromArgs(args)
                         .withValidation()
                         .as(FlinkPipelineOptions.class);
         options.setRunner(FlinkRunner.class);
         options.setParallelism(4);
 
-        var pipeline = Pipeline.create(options);
+        List<String> sensorValues =
+                Arrays.asList(
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 25.3, \"t\": 1645467200}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 25.7, \"t\": 1645467500}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 26.2, \"t\": 1645467800}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 26.8, \"t\": 1645468100}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 27.1, \"t\": 1645468400}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 27.5, \"t\": 1645468700}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 27.9, \"t\": 1645469000}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 28.3, \"t\": 1645469300}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 28.7, \"t\": 1645469600}",
+                        "{\"bn\": \"temperature_sensor\", \"n\": \"temperature\", \"u\": \"°C\","
+                                + " \"v\": 29.2, \"t\": 1645469900}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 50, \"t\": 1645467200}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 51, \"t\": 1645467500}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 52, \"t\": 1645467800}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 53, \"t\": 1645468100}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 54, \"t\": 1645468400}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 55, \"t\": 1645468700}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 56, \"t\": 1645469000}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 57, \"t\": 1645469300}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 58, \"t\": 1645469600}",
+                        "{\"bn\": \"humidity_sensor\", \"n\": \"humidity\", \"u\": \"%\", \"v\":"
+                                + " 59, \"t\": 1645469900}");
 
-        PCollection<Long> numbers = pipeline.apply(GenerateSequence.from(1).to(10));
+        Pipeline pipeline = Pipeline.create(options);
 
-        PCollection<Long> filteredNumbers = numbers.apply(Filter.by(number -> number % 2 != 0));
+        PCollection<String> senMLStrings = pipeline.apply(Create.of(sensorValues));
 
-        PCollection<Long> filteredNumbersLT = filteredNumbers.apply(Filter.by(n -> n > 5));
+        PCollection<SenMLRecord> senMLRecords =
+                senMLStrings.apply(
+                        MapElements.into(TypeDescriptor.of(SenMLRecord.class))
+                                .via(SenMLRecord::new));
 
-        PCollection<String> output =
-                filteredNumbersLT.apply(
-                        MapElements.into(TypeDescriptors.strings()).via(Object::toString));
+        PCollection<Instant> sensorValueTimestamps =
+                senMLRecords.apply(
+                        MapElements.into(TypeDescriptor.of(Instant.class))
+                                .via(SenMLRecord::getTime));
 
-        output.apply(ParDo.of(new PrintFn()));
+        PCollection<String> timestampStrings =
+                sensorValueTimestamps.apply(
+                        MapElements.into(TypeDescriptors.strings()).via(Objects::toString));
+
+        timestampStrings.apply(ParDo.of(new PrintFn()));
 
         pipeline.run();
     }

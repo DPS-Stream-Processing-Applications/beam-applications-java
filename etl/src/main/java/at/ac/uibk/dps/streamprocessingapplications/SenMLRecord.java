b71c8a9338b51b277db5311af567f2278679cdb9
eprader <56026248+eprader@users.noreply.github.com>
2024-02-27 22:49:53 +0100

    formatting

--- etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/SenMLRecord.java
+++ etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/SenMLRecord.java
@@ -20,63 +20,61 @@ import org.json.JSONObject;
  * </ul>
  */
 public class SenMLRecord implements Serializable {
-    @Nullable private String baseName;
+  @Nullable private String baseName;
 
-    @Nullable private String name;
-    @Nullable private String unit;
-    @Nullable private Float value;
-    @Nullable private Instant time;
+  @Nullable private String name;
+  @Nullable private String unit;
+  @Nullable private Float value;
+  @Nullable private Instant time;
 
-    public SenMLRecord(String senMLString) {
-        JSONObject record = new JSONObject(senMLString);
-        this.baseName = record.has("bn") ? record.optString("bn") : null;
-        this.name = record.has("n") ? record.optString("n") : null;
-        this.unit = record.has("u") ? record.optString("u") : null;
-        this.value = record.has("v") ? Float.valueOf(record.optString("v")) : null;
-        this.time =
-                record.has("t")
-                        ? Instant.ofEpochSecond(Long.parseLong(record.optString("t")))
-                        : null;
-    }
+  public SenMLRecord(String senMLString) {
+    JSONObject record = new JSONObject(senMLString);
+    this.baseName = record.has("bn") ? record.optString("bn") : null;
+    this.name = record.has("n") ? record.optString("n") : null;
+    this.unit = record.has("u") ? record.optString("u") : null;
+    this.value = record.has("v") ? Float.valueOf(record.optString("v")) : null;
+    this.time =
+        record.has("t") ? Instant.ofEpochSecond(Long.parseLong(record.optString("t"))) : null;
+  }
 
-    public String getBaseName() {
-        return baseName;
-    }
+  public String getBaseName() {
+    return baseName;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getUnit() {
-        return unit;
-    }
+  public String getUnit() {
+    return unit;
+  }
 
-    public Float getValue() {
-        return value;
-    }
+  public Float getValue() {
+    return value;
+  }
 
-    public Instant getTime() {
-        return time;
-    }
+  public Instant getTime() {
+    return time;
+  }
 
-    public String getFullName() {
-        return this.getBaseName() + this.getName();
-    }
+  public String getFullName() {
+    return this.getBaseName() + this.getName();
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof SenMLRecord)) return false;
-        SenMLRecord that = (SenMLRecord) o;
-        return Objects.equals(getBaseName(), that.getBaseName())
-                && Objects.equals(getName(), that.getName())
-                && Objects.equals(getUnit(), that.getUnit())
-                && Objects.equals(getValue(), that.getValue())
-                && Objects.equals(getTime(), that.getTime());
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof SenMLRecord)) return false;
+    SenMLRecord that = (SenMLRecord) o;
+    return Objects.equals(getBaseName(), that.getBaseName())
+        && Objects.equals(getName(), that.getName())
+        && Objects.equals(getUnit(), that.getUnit())
+        && Objects.equals(getValue(), that.getValue())
+        && Objects.equals(getTime(), that.getTime());
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getBaseName(), getName(), getUnit(), getValue(), getTime());
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(getBaseName(), getName(), getUnit(), getValue(), getTime());
+  }
 }

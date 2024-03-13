3c33ceb9a1b2823221005e512322fe811fa51fa6
Jungerindy <jona.indy@gmail.com>
2024-03-13 12:07:09 +0100

    rename .conf file

--- train/src/main/java/at/ac/uibk/dps/streamprocessingapplications/TrainJob.java
+++ train/src/main/java/at/ac/uibk/dps/streamprocessingapplications/TrainJob.java
@@ -27,7 +27,7 @@ public class TrainJob {
         }
         return lines;
     }
-
+    
     public static void main(String[] args) throws Exception {
 
         ArgumentClass argumentClass = ArgumentParser.parserCLI(args);

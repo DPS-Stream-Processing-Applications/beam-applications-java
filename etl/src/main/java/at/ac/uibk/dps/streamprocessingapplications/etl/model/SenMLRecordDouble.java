98cbf03e3011905a50af88f9e5e56c6e3e42a89d
eprader <56026248+eprader@users.noreply.github.com>
2024-04-01 17:48:20 +0200

    records: move to optional add to remove unused fields if null

--- etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/etl/model/SenMLRecordDouble.java
+++ etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/etl/model/SenMLRecordDouble.java
@@ -1,7 +1,7 @@
 package at.ac.uibk.dps.streamprocessingapplications.etl.model;
 
 import java.time.Instant;
-import java.util.Map;
+import java.util.Optional;
 import javax.annotation.Nullable;
 import org.json.JSONObject;
 
@@ -18,18 +18,14 @@ public class SenMLRecordDouble extends AbstractSenMLRecord<Double> {
 
   @Override
   public String toString() {
-    return new JSONObject(
-            Map.of(
-                "bn",
-                this.getBaseName(),
-                "n",
-                this.getName(),
-                "u",
-                this.getUnit(),
-                "v",
-                this.getValue(),
-                "t",
-                this.getTime()))
-        .toString();
+    JSONObject jsonObject = new JSONObject();
+
+    Optional.ofNullable(getBaseName()).ifPresent(val -> jsonObject.put("bn", val));
+    Optional.ofNullable(getName()).ifPresent(val -> jsonObject.put("n", val));
+    Optional.ofNullable(getUnit()).ifPresent(val -> jsonObject.put("u", val));
+    Optional.ofNullable(getValue()).ifPresent(val -> jsonObject.put("v", val));
+    Optional.ofNullable(getTime()).ifPresent(val -> jsonObject.put("t", val));
+
+    return jsonObject.toString();
   }
 }
--- etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/etl/model/SenMLRecordString.java
+++ etl/src/main/java/at/ac/uibk/dps/streamprocessingapplications/etl/model/SenMLRecordString.java
@@ -1,7 +1,7 @@
 package at.ac.uibk.dps.streamprocessingapplications.etl.model;
 
 import java.time.Instant;
-import java.util.Map;
+import java.util.Optional;
 import javax.annotation.Nullable;
 import org.json.JSONObject;
 
@@ -15,20 +15,16 @@ public class SenMLRecordString extends AbstractSenMLRecord<String> {
     super(baseName, name, unit, value, time);
   }
 
-  @Override
-  public String toString() {
-    return new JSONObject(
-            Map.of(
-                "bn",
-                this.getBaseName(),
-                "n",
-                this.getName(),
-                "u",
-                this.getUnit(),
-                "vs",
-                this.getValue(),
-                "t",
-                this.getTime()))
-        .toString();
-  }
+    @Override
+    public String toString() {
+        JSONObject jsonObject = new JSONObject();
+
+        Optional.ofNullable(getBaseName()).ifPresent(val -> jsonObject.put("bn", val));
+        Optional.ofNullable(getName()).ifPresent(val -> jsonObject.put("n", val));
+        Optional.ofNullable(getUnit()).ifPresent(val -> jsonObject.put("u", val));
+        Optional.ofNullable(getValue()).ifPresent(val -> jsonObject.put("vs", val));
+        Optional.ofNullable(getTime()).ifPresent(val -> jsonObject.put("t", val));
+
+        return jsonObject.toString();
+    }
 }

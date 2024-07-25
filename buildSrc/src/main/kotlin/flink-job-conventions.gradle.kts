39cafa829ac2bb257857bcf9c23f7a0b19b9c8ab
eprader <56026248+eprader@users.noreply.github.com>
2024-07-26 00:33:01 +0200

    gradle(flink-job-conventions): bump `Flink` version to `1.18`, `Beam` version to `2.57.0`, add missing `junit-jupiter` dependencies

--- buildSrc/src/main/kotlin/flink-job-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/flink-job-conventions.gradle.kts
@@ -14,27 +14,41 @@ repositories {
     }
 }
 
-dependencies {
-    implementation("org.apache.beam:beam-sdks-java-core:2.54.0")
-    implementation("org.apache.beam:beam-runners-direct-java:2.54.0")
-    implementation("org.apache.beam:beam-runners-flink-1.16:2.54.0")
-    implementation("org.apache.beam:beam-sdks-java-io-kafka:2.54.0")
-    implementation("org.apache.beam:beam-sdks-java-io-mongodb:2.54.0")
-    implementation("org.slf4j:slf4j-jdk14:1.7.32")
-    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
-    /* INFO:
-     * The beam `KafkaIO` does not specify the kafka client which is needed to interact with the kafka topics.
-     * Instead, one needs to add the `kafka-clients` dependency
-     * with the version of the kafka cluster to be interfaced with.
-     *
-     * For this implementation it needs to match the kafka version in `kafka-cluster.yaml`.
-     */
-    implementation("org.apache.kafka:kafka-clients:3.7.0")
+/* WARN:
+ * The beam `KafkaIO` does not specify the kafka client which is needed to interact with the kafka topics.
+ * Instead, one needs to add the `kafka-clients` dependency
+ * with the version of the kafka cluster to be interfaced with.
+ *
+ * For this implementation it needs to match the kafka version in `kafka-cluster.yaml`.
+ */
+val kafkaClientsVersion = "3.7.0"
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+val flinkVersion = "1.18"
+val beamVersion = "2.57.0"
+val slf4jVersion = "1.7.32"
+val log4jVersion = "2.23.1"
+val junitJupiterVersion = "5.10.3"
+val hamcrestVersion = "2.2"
+
+dependencies {
+    implementation("org.apache.beam:beam-sdks-java-core:$beamVersion")
+    implementation("org.apache.beam:beam-runners-direct-java:$beamVersion")
+    implementation("org.apache.beam:beam-runners-flink-$flinkVersion:$beamVersion")
+    implementation("org.apache.beam:beam-sdks-java-io-kafka:$beamVersion")
+    implementation("org.apache.beam:beam-sdks-java-io-mongodb:$beamVersion")
+    implementation("org.slf4j:slf4j-jdk14:$slf4jVersion")
+    implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
+    implementation("org.apache.kafka:kafka-clients:$kafkaClientsVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testImplementation("org.junit.vintage:junit-vintage-engine:$junitJupiterVersion")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.hamcrest:hamcrest:2.2")
+
+    testImplementation("org.hamcrest:hamcrest:$hamcrestVersion")
+
 }
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11

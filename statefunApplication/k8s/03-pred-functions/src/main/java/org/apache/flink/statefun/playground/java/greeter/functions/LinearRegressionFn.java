6add5e40350832238f0e72e7eb0aa9cbfd672e03
Jungerindy <jona.indy@gmail.com>
2024-08-22 13:43:27 +0200

    trying to fix statefun crash

--- statefunApplication/k8s/03-pred-functions/src/main/java/org/apache/flink/statefun/playground/java/greeter/functions/LinearRegressionFn.java
+++ statefunApplication/k8s/03-pred-functions/src/main/java/org/apache/flink/statefun/playground/java/greeter/functions/LinearRegressionFn.java
@@ -51,12 +51,15 @@ public class LinearRegressionFn implements StatefulFunction {
             throw new RuntimeException(e);
         }
         this.dataSetType = datasetType;
+        /*
         if (linearRegressionPredictor == null) {
             String ipAddress = System.getenv("MONGO_DB_ADDRESS");
             linearRegressionPredictor = new LinearRegressionPredictor(ipAddress, "mydb");
             initLogger(LoggerFactory.getLogger("APP"));
             linearRegressionPredictor.setup(l, p);
         }
+
+         */
     }
 
 
@@ -84,8 +87,8 @@ public class LinearRegressionFn implements StatefulFunction {
 
                 HashMap<String, String> map = new HashMap<>();
                 map.put(AbstractTask.DEFAULT_KEY, obsVal);
-                Float res = linearRegressionPredictor.doTask(map);
-                //Float res = 2.0f;
+                //Float res = linearRegressionPredictor.doTask(map);
+                Float res = 2.0f;
 
                 if (l.isInfoEnabled()) l.info("res linearRegressionPredictor-" + res);
 

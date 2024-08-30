d5d4afadab6fcbfb4900038b67e429782bb4ef02
Jungerindy <jona.indy@gmail.com>
2024-08-30 16:29:10 +0200

    changes in statefunStarter

---
+++ statefunStarter/README.md
@@ -0,0 +1,31 @@
+# StatefunStarter
+## General idea
+The StatefunStarter starts the statefun-functions, when a record arrives for the functions and shuts it down if there are no incoming records.
+The Kafkaproducer sends records to a topic (statefun-starter-input), and these records are processed by the statefun functions. The system includes an automatic shutdown feature that terminates idle services after 60 seconds of inactivity, ensuring efficient resource utilization.
+
+## Setup steps
+
+>[!WARNING]
+> Make sure to adapt the values ``Dataset`` and ``MONGODB_ADDRESS`` in the ``statefunStarter-manifest.yaml``
+
+
+
+```bash
+kubectl apply -f service-account.yaml
+```
+
+```bash
+kubectl apply -f updated-role.yaml
+```
+
+```bash
+kubectl apply -f updated-rolebinding.yaml
+```
+
+```bash
+kubectl apply -f kafka-topic-statefun-starter-input.yaml
+```
+
+```bash
+kubectl apply -f statefunStarter-manifest.yaml -n statefun
+```
--- statefunStarter/statefunStarter.py
+++ statefunStarter/statefunStarter.py
@@ -5,37 +5,66 @@ import time
 import yaml
 import os
 import requests
+import logging
+import sys
+import signal
 
 
 consumer = KafkaConsumer(
-    "topicA", bootstrap_servers=["kafka-cluster-kafka-bootstrap.default.svc:9092"]
+    "statefun-starter-input",
+    bootstrap_servers=["kafka-cluster-kafka-bootstrap.default.svc:9092"],
 )
 producer = KafkaProducer(
     bootstrap_servers=["kafka-cluster-kafka-bootstrap.default.svc:9092"],
     key_serializer=lambda k: k.encode("utf-8") if isinstance(k, str) else k,
     value_serializer=lambda v: v.encode("utf-8") if isinstance(v, str) else v,
 )
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+
+def handle_sigterm(signum, frame):
+    logging.info("Received SIGTERM signal. Shutting down gracefully...")
+    cleanup()
+    exit(0)
+
+
+def cleanup():
+    try:
+        consumer.close()
+        producer.close()
+        terminate_deployment_and_service(manifest_docs)
+    except Exception as e:
+        logging.info(f"Cleanup error: {e}")
+        logging.info("functions pod was already down")
 
 
 def check_if_service_is_already_running(name):
     k8s_core_v1 = client.CoreV1Api()
     try:
         k8s_core_v1.read_namespaced_service(name=name, namespace="statefun")
-        print(f"Service '{name}' already exists. Skipping creation.")
+        logging.info(f"Service '{name}' already exists. Skipping creation.")
     except ApiException as e:
         if e.status == 404:
-            print(f"Service '{name}'. is new")
+            logging.info(f"Service '{name}'. is new")
 
 
 def read_metric_from_prometheus(metric_name):
     prometheus_url = "http://localhost:9090"
-    response = requests.get(
-        f"{prometheus_url}/api/v1/query",
-        params={"query": metric_name},
-    )
-    data = response.json()
-    value = data["data"]["result"][0]["value"][1]
-    return int(value)
+    try:
+        response = requests.get(
+            f"{prometheus_url}/api/v1/query",
+            params={"query": metric_name},
+        )
+        data = response.json()
+        value = data["data"]["result"][0]["value"][1]
+        return int(value)
+    except Exception as e:
+        logging.error(f"Error, when reading from prometheus: {e}")
+        return 0
 
 
 def check_if_pod_is_idle(metric_name, num_records_in, application):
@@ -79,18 +108,18 @@ def wait_for_deployment_and_service(
         if is_deployment_ready(
             k8s_apps_v1, deployment_name, namespace
         ) and is_service_ready(k8s_core_v1, service_name, namespace):
-            print(
+            logging.info(
                 f"Deployment '{deployment_name}' is ready and Service '{service_name}' is ready."
             )
             return True
         time.sleep(interval)
-    print(
+    logging.error(
         f"Timeout reached. Deployment '{deployment_name}' or Service '{service_name}' not ready."
     )
     return False
 
 
-def read_manifest(path_manifest, mongodb_address):
+def read_manifest(path_manifest, mongodb_address, dataset):
     with open(path_manifest, "r") as f:
         manifest = list(yaml.safe_load_all(f))
     for item in manifest:
@@ -101,16 +130,19 @@ def read_manifest(path_manifest, mongodb_address):
                 for env in env_vars:
                     if env["name"] == "MONGO_DB_ADDRESS":
                         env["value"] = mongodb_address
+                    elif env["name"] == "DATASET":
+                        env["value"] = dataset
     return manifest
 
 
 def start_deployment_and_service(message, path_manifest):
-    print("Starting deployment and service")
+    logging.info("Starting deployment and service")
     config.load_incluster_config()
     k8s_core_v1 = client.CoreV1Api()
     k8s_apps_v1 = client.AppsV1Api()
     deployment_name = None
     service_name = None
+    start_time = time.time()
     for doc in path_manifest:
         kind = doc.get("kind")
         metadata = doc.get("metadata", {})
@@ -120,7 +152,7 @@ def start_deployment_and_service(message, path_manifest):
             resp = k8s_apps_v1.create_namespaced_deployment(
                 body=doc, namespace="statefun"
             )
-            print(
+            logging.info(
                 f"Deployment '{deployment_name}' created. Status='{resp.metadata.name}'"
             )
         elif kind == "Service":
@@ -128,7 +160,9 @@ def start_deployment_and_service(message, path_manifest):
             resp = k8s_core_v1.create_namespaced_service(
                 body=doc, namespace="statefun", pretty="true"
             )
-            print(f"Service '{service_name}' created. Status='{resp.metadata.name}'")
+            logging.info(
+                f"Service '{service_name}' created. Status='{resp.metadata.name}'"
+            )
 
     if deployment_name and service_name:
         if wait_for_deployment_and_service(
@@ -139,10 +173,14 @@ def start_deployment_and_service(message, path_manifest):
                 key=str(int(time.time() * 1000)).encode("utf-8"),
                 value=message.decode(),
             )
+            end_time = time.time()
+            duration = end_time - start_time
+            logging.info(f"Time taken to create deployment: {duration:.2f} seconds")
+
         else:
-            print("Deployment or Service did not become ready in time.")
+            logging.error("Deployment or Service did not become ready in time.")
     else:
-        print("Deployment or Service name not found in manifest.")
+        logging.error("Deployment or Service name not found in manifest.")
 
 
 def terminate_deployment_and_service(manifest_docs):
@@ -157,12 +195,12 @@ def terminate_deployment_and_service(manifest_docs):
             resp = k8s_apps_v1.delete_namespaced_deployment(
                 name=name, namespace="statefun"
             )
-            print(f"Deployment '{name}' deleted")
+            logging.info(f"Deployment '{name}' deleted")
         elif kind == "Service":
             resp = k8s_core_v1.delete_namespaced_service(
                 name=name, namespace="statefun"
             )
-            print(f"Service '{name}' deleted")
+            logging.info(f"Service '{name}' deleted")
 
 
 def main(manifest_docs, metric_name, application):
@@ -195,9 +233,13 @@ def main(manifest_docs, metric_name, application):
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, handle_sigterm)
     try:
         application = os.getenv("APPLICATION")
         mongodb_address = os.getenv("MONGODB")
+        dataset = os.getenv("DATASET")
+        if not (dataset == "FIT" or dataset == "SYS" or dataset == "TAXI"):
+            raise Exception("Unsupported dataset argument")
         metric_name = None
         if application == "TRAIN":
             metric_name = "flink_taskmanager_job_task_operator_functions_pred_mqttPublishTrain_outEgress"
@@ -205,14 +247,17 @@ if __name__ == "__main__":
         if application == "PRED":
             metric_name = "flink_taskmanager_job_task_operator_functions_pred_mqttPublish_outEgress"
             path_manifest = "/app/pred/functions-service.yaml"
-        manifest_docs = read_manifest(path_manifest, mongodb_address)
+        manifest_docs = read_manifest(path_manifest, mongodb_address, dataset)
         main(manifest_docs, metric_name, application)
     except KeyboardInterrupt:
-        print("Shutting down")
+        logging.info("Shutting down")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+
     finally:
         consumer.close()
         producer.close()
         try:
             terminate_deployment_and_service(manifest_docs)
         except:
-            print("functions pod was already down")
+            logging.info("functions pod was already down")

67720fa78cd15905235310cedad09dedc5477827
Jungerindy <jona.indy@gmail.com>
2024-08-30 16:58:33 +0200

    adapt MqttPublish-Function for train-statefun

--- statefunApplication/k8s/03-train-functions/src/main/java/org/apache/flink/statefun/playground/java/greeter/functions/MqttPublishTrainFn.java
+++ statefunApplication/k8s/03-train-functions/src/main/java/org/apache/flink/statefun/playground/java/greeter/functions/MqttPublishTrainFn.java
@@ -1,78 +1,61 @@
 package org.apache.flink.statefun.playground.java.greeter.functions;
 
-import org.apache.flink.statefun.playground.java.greeter.kafka.MyKafkaProducer;
-import org.apache.flink.statefun.playground.java.greeter.tasks.AbstractTask;
 import org.apache.flink.statefun.playground.java.greeter.types.BlobUploadEntry;
 import org.apache.flink.statefun.playground.java.greeter.types.MqttPublishEntry;
 import org.apache.flink.statefun.sdk.java.Context;
 import org.apache.flink.statefun.sdk.java.StatefulFunction;
 import org.apache.flink.statefun.sdk.java.StatefulFunctionSpec;
 import org.apache.flink.statefun.sdk.java.TypeName;
+import org.apache.flink.statefun.sdk.java.io.KafkaEgressMessage;
 import org.apache.flink.statefun.sdk.java.message.Message;
 import org.apache.flink.statefun.sdk.java.message.MessageBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
 
 import static org.apache.flink.statefun.playground.java.greeter.types.Types.BlobUpload_ENTRY_JSON_TYPE;
 import static org.apache.flink.statefun.playground.java.greeter.types.Types.MqttPublish_ENTRY_JSON_TYPE;
 
 public class MqttPublishTrainFn implements StatefulFunction {
+
+
     static final TypeName TYPENAME = TypeName.typeNameFromString("pred/mqttPublishTrain");
     public static final StatefulFunctionSpec SPEC =
             StatefulFunctionSpec.builder(TYPENAME)
                     .withSupplier(MqttPublishTrainFn::new)
                     .build();
     static final TypeName INBOX = TypeName.typeNameFromString("pred/sinkTrain");
+    static final TypeName KAFKA_EGRESS = TypeName.typeNameFromString("pred/publish");
     private static Logger l;
-    MyKafkaProducer myKafkaProducer;
-    String server;
-    String topic;
-    private Properties p;
-
     public static void initLogger(Logger l_) {
         l = l_;
     }
 
-
-    public void setup() {
-        p = new Properties();
-        try (InputStream input = Files.newInputStream(Paths.get("/resources/all_tasks.properties"))) {
-            p.load(input);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        this.server = "kafka-cluster-kafka-bootstrap.default.svc:9092";
-        this.topic = "train-publish";
+    public CompletableFuture<Void> apply(Context context, Message message) {
         initLogger(LoggerFactory.getLogger("APP"));
-        myKafkaProducer = new MyKafkaProducer(server, topic, p);
-        myKafkaProducer.setup(l, p);
-    }
+        try {
+            BlobUploadEntry blobUploadEntry = message.as(BlobUpload_ENTRY_JSON_TYPE);
+            String msgId = blobUploadEntry.getMsgid();
+            String filename = blobUploadEntry.getFileName();
+            MqttPublishEntry mqttPublishEntry = new MqttPublishEntry(msgId);
+            mqttPublishEntry.setArrivalTime(blobUploadEntry.getArrivalTime());
+            context.send(
+                    MessageBuilder.forAddress(INBOX, String.valueOf(mqttPublishEntry.getMsgid()))
+                            .withCustomType(MqttPublish_ENTRY_JSON_TYPE, mqttPublishEntry)
+                            .build());
+            context.send(
+                    KafkaEgressMessage.forEgress(KAFKA_EGRESS)
+                            .withTopic("pred-publish")
+                            .withUtf8Key(String.valueOf(System.currentTimeMillis()))
+                            .withUtf8Value(String.valueOf(filename))
+                            .build());
 
 
-    @Override
-    public CompletableFuture<Void> apply(Context context, Message message) throws Throwable {
-        setup();
-        BlobUploadEntry blobUploadEntry = message.as(BlobUpload_ENTRY_JSON_TYPE);
-        String msgId = blobUploadEntry.getMsgid();
-        String filename = blobUploadEntry.getFileName();
-
-        HashMap<String, String> map = new HashMap<>();
-        map.put(AbstractTask.DEFAULT_KEY, filename);
-        myKafkaProducer.doTask(map);
-        MqttPublishEntry mqttPublishEntry = new MqttPublishEntry(msgId);
-        mqttPublishEntry.setArrivalTime(blobUploadEntry.getArrivalTime());
-        context.send(
-                MessageBuilder.forAddress(INBOX, String.valueOf(mqttPublishEntry.getMsgid()))
-                        .withCustomType(MqttPublish_ENTRY_JSON_TYPE, mqttPublishEntry)
-                        .build());
-
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error in ParsePredictBeam " + e);
+        }
         return context.done();
     }
 }

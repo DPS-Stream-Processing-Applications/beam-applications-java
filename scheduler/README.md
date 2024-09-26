98e37cd8293eaaaa5f0a0dfb831a77f0e68a0d30
Jungerindy <jona.indy@gmail.com>
2024-09-26 21:24:51 +0200

    add serverless functionality to the scheduler prototype

---
+++ scheduler/README.md
@@ -0,0 +1,18 @@
+## Kubernetes Setup Instructions
+
+Follow the steps below to apply the necessary roles, role bindings, service accounts, and cluster roles in your Kubernetes environment.
+
+### Step 1: Apply Role and RoleBinding for Default Namespace
+
+```bash
+kubectl apply -f role.yaml
+kubectl apply -f rolebinding.yaml
+kubectl apply -f service-account.yaml
+```
+
+```bash
+kubectl apply -f cluster_role.yaml
+kubectl apply -f cluster_role_binding.yaml
+kubectl apply -f service-account-statefun.yaml
+```
+
--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -27,6 +27,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 forwarder = None
+is_serverful_framework_used = True
 
 
 def handle_sigterm(signum, frame):
@@ -40,7 +41,7 @@ def cleanup():
     try:
         consumer.close()
         producer.close()
-        terminate_deployment(manifest_docs)
+        terminate_flink_deployment(manifest_docs)
         stop_port_forward(forwarder)
     except Exception as e:
         logging.info(f"Cleanup error: {e}")
@@ -129,7 +130,7 @@ def is_flink_deployment_ready(
     return False
 
 
-def wait_for_flinkdeployment(
+def wait_for_flink_deployment(
     k8s_apps_v1,
     deployment_name,
     namespace="default",
@@ -146,13 +147,13 @@ def wait_for_flinkdeployment(
     return False
 
 
-def read_manifest_serverful(path_manifest):
+def read_manifest(path_manifest):
     with open(path_manifest, "r") as f:
         manifest = list(yaml.safe_load_all(f))
     return manifest
 
 
-def start_deployment(path_manifest):
+def start_flink_deployment(path_manifest):
     logging.info("Starting flink-session-cluster")
     config.load_incluster_config()
     k8s_custom_objects_api = client.CustomObjectsApi()
@@ -175,7 +176,7 @@ def start_deployment(path_manifest):
                 f"FlinkDeployment '{deployment_name}' created. Status='{resp['metadata']['name']}'"
             )
     if deployment_name:
-        if wait_for_flinkdeployment(k8s_custom_objects_api, deployment_name):
+        if wait_for_flink_deployment(k8s_custom_objects_api, deployment_name):
             end_time = time.time()
             duration = end_time - start_time
             logging.info(
@@ -188,7 +189,7 @@ def start_deployment(path_manifest):
         logging.error("Deployment name not found in manifest.")
 
 
-def terminate_deployment(manifest_docs):
+def terminate_flink_deployment(manifest_docs):
     config.load_incluster_config()
     k8s_custom_objects_api = client.CustomObjectsApi()
 
@@ -270,6 +271,219 @@ def stop_flink_job(job_manager_host, job_id):
         print(f"An error occurred: {e}")
 
 
+def start_deployment_and_service(path_manifest, is_statefun_starter=False):
+    logging.info("Starting deployment and service")
+    config.load_incluster_config()
+    k8s_core_v1 = client.CoreV1Api()
+    k8s_apps_v1 = client.AppsV1Api()
+    deployment_name = None
+    service_name = None
+    start_time = time.time()
+    for doc in path_manifest:
+        kind = doc.get("kind")
+        metadata = doc.get("metadata", {})
+        name = metadata.get("name")
+        if kind == "Deployment":
+            deployment_name = name
+            resp = k8s_apps_v1.create_namespaced_deployment(
+                body=doc, namespace="statefun"
+            )
+            logging.info(
+                f"Deployment '{deployment_name}' created. Status='{resp.metadata.name}'"
+            )
+        elif kind == "Service":
+            service_name = name
+            resp = k8s_core_v1.create_namespaced_service(
+                body=doc, namespace="statefun", pretty="true"
+            )
+            logging.info(
+                f"Service '{service_name}' created. Status='{resp.metadata.name}'"
+            )
+        elif kind == "ConfigMap":
+            config_map_name = name
+            resp = k8s_core_v1.create_namespaced_config_map(
+                body=doc, namespace="statefun"
+            )
+            logging.info(
+                f"ConfigMap '{config_map_name}' created. Status='{resp.metadata.name}'"
+            )
+
+    if deployment_name and service_name and not is_statefun_starter:
+        if wait_for_deployment_and_service(
+            k8s_apps_v1, k8s_core_v1, deployment_name, service_name
+        ):
+            end_time = time.time()
+            duration = end_time - start_time
+            logging.info(f"Time taken to create deployment: {duration:.2f} seconds")
+    elif deployment_name and is_statefun_starter:
+        if wait_for_deployment(k8s_apps_v1, deployment_name):
+            end_time = time.time()
+            duration = end_time - start_time
+            logging.info(f"Time taken to create deployment: {duration:.2f} seconds")
+        else:
+            logging.error("Deployment or Service did not become ready in time.")
+    else:
+        logging.error("Deployment or Service name not found in manifest.")
+
+
+def wait_for_deployment_and_service(
+    k8s_apps_v1,
+    k8s_core_v1,
+    deployment_name,
+    service_name,
+    namespace="statefun",
+    timeout=300,
+    interval=5,
+):
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        if is_deployment_ready(
+            k8s_apps_v1, deployment_name, namespace
+        ) and is_service_ready(k8s_core_v1, service_name, namespace):
+            logging.info(
+                f"Deployment '{deployment_name}' is ready and Service '{service_name}' is ready."
+            )
+            return True
+        time.sleep(interval)
+    logging.error(
+        f"Timeout reached. Deployment '{deployment_name}' or Service '{service_name}' not ready."
+    )
+    return False
+
+
+def wait_for_deployment(
+    k8s_apps_v1,
+    deployment_name,
+    namespace="statefun",
+    timeout=300,
+    interval=5,
+):
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        if is_deployment_ready(k8s_apps_v1, deployment_name, namespace):
+            logging.info(f"Deployment '{deployment_name}' is ready.")
+            return True
+        time.sleep(interval)
+    logging.error(f"Timeout reached. Deployment '{deployment_name} not ready.")
+    return False
+
+
+def is_deployment_ready(k8s_apps_v1, deployment_name, namespace="statefun"):
+    deployment = k8s_apps_v1.read_namespaced_deployment(
+        name=deployment_name, namespace=namespace
+    )
+    return deployment.status.ready_replicas == deployment.spec.replicas
+
+
+def is_service_ready(k8s_core_v1, service_name, namespace="statefun"):
+    endpoints = k8s_core_v1.read_namespaced_endpoints(
+        name=service_name, namespace=namespace
+    )
+    return len(endpoints.subsets) > 0
+
+
+def create_minio():
+    manifest = read_manifest("/app/minio.yaml")
+    start_deployment_and_service(manifest)
+
+
+def create_statefun_environment():
+    # Note this is a ConfigMap
+    manifest_module = read_manifest("/app/00-module.yaml")
+    manifest_runtime = read_manifest("/app/01-statefun-runtime.yaml")
+    start_deployment_and_service(manifest_module)
+    start_deployment_and_service(manifest_runtime)
+
+
+def create_statefun_starter():
+    # FIXME
+    manifest = read_manifest_statefun_starter("/app/statefunStarter-manifest.yaml", True)
+    start_deployment_and_service(manifest, True)
+
+
+def delete_minio():
+    manifest = read_manifest("/app/minio.yaml")
+    terminate_deployment_and_service(manifest)
+
+
+def delete_statefun_environment():
+    manifest_module = read_manifest("/app/00-module.yaml")
+    manifest_runtime = read_manifest("/app/01-statefun-runtime.yaml")
+    terminate_deployment_and_service(manifest_module)
+    terminate_deployment_and_service(manifest_runtime)
+
+
+def delete_statefun_starter():
+    manifest = read_manifest("/app/statefunStarter-manifest.yaml")
+    terminate_deployment_and_service(manifest)
+
+
+def terminate_serverless_framework():
+    delete_statefun_starter()
+    delete_statefun_environment()
+    delete_minio()
+
+
+def create_serverless_framework():
+    create_minio()
+    create_statefun_environment()
+    create_statefun_starter()
+
+
+def terminate_deployment_and_service(manifest_docs):
+    config.load_incluster_config()
+    k8s_core_v1 = client.CoreV1Api()
+    k8s_apps_v1 = client.AppsV1Api()
+    for doc in manifest_docs:
+        kind = doc.get("kind")
+        metadata = doc.get("metadata", {})
+        name = metadata.get("name")
+        if kind == "Deployment":
+            resp = k8s_apps_v1.delete_namespaced_deployment(
+                name=name, namespace="statefun"
+            )
+            logging.info(f"Deployment '{name}' deleted")
+        elif kind == "Service":
+            resp = k8s_core_v1.delete_namespaced_service(
+                name=name, namespace="statefun"
+            )
+            logging.info(f"Service '{name}' deleted")
+        elif kind == "ConfigMap":
+            resp = k8s_core_v1.delete_namespaced_config_map(
+                name=name, namespace="statefun"
+            )
+            logging.info(f"ConfigMap '{name}' deleted.")
+
+
+def read_manifest_statefun_starter(path_manifest, run_locally=False):
+    with open(path_manifest, "r") as f:
+        manifest = list(yaml.safe_load_all(f))
+    for item in manifest:
+        if item["kind"] == "Deployment":
+            containers = item["spec"]["template"]["spec"]["containers"]
+            for container in containers:
+                if run_locally:
+                    container["imagePullPolicy"] = "IfNotPresent"
+                else:
+                    container["imagePullPolicy"] = "Always"
+                env_vars = container.get("env", [])
+                for env in env_vars:
+                    if env["name"] == "MONGO_DB_ADDRESS":
+                        env["value"] = os.getenv("MONGODB")
+                    elif env["name"] == "DATASET":
+                        env["value"] = os.getenv("DATASET")
+                    elif env["name"] == "APPLICATION":
+                        env["value"] = os.getenv("APPLICATION")
+    return manifest
+
+
+def delete_all_jobs_from_serverful_framework():
+    list_of_running_jobs = get_jobid_of_running_job("flink-session-cluster-rest")
+    for job in list_of_running_jobs:
+        stop_flink_job("flink-session-cluster-rest", job)
+        logging.info(f"Deleted job {job}")
+
+
 def main(manifest_docs, application):
     is_deployed = False
     number_sent_messages = 0
@@ -279,7 +493,7 @@ def main(manifest_docs, application):
             for tp, messages in message.items():
                 for msg in messages:
                     if not is_deployed:
-                        start_deployment(manifest_docs)
+                        start_flink_deployment(manifest_docs)
                         is_deployed = True
                     last_message_time = time.time()
                     number_sent_messages = number_sent_messages + 1
@@ -301,11 +515,11 @@ def debug_main(manifest_docs, application, dataset):
         dataset + "-120",
     )
     """
-    res = get_jobid_of_running_job("flink-session-cluster-rest")
-    logging.info("Res: " + str(res))
-    for job in res:
-        stop_flink_job("flink-session-cluster-rest", job)
-
+    is_deployed = False
+    number_sent_messages = 0
+    if not is_deployed:
+        create_serverless_framework()
+        logging.info("Exiting debug_main")
 
 if __name__ == "__main__":
     signal.signal(signal.SIGTERM, handle_sigterm)
@@ -316,7 +530,7 @@ if __name__ == "__main__":
         if not (dataset == "FIT" or dataset == "SYS" or dataset == "TAXI"):
             raise Exception("Unsupported dataset argument")
         path_manifest = "/app/flink-session-cluster-deployment.yaml"
-        manifest_docs = read_manifest_serverful(path_manifest)
+        manifest_docs = read_manifest(path_manifest)
         # main(manifest_docs, application)
         debug_main(manifest_docs, application, dataset)
     except KeyboardInterrupt:
@@ -328,7 +542,7 @@ if __name__ == "__main__":
         consumer.close()
         producer.close()
         try:
-            pass
+            terminate_serverless_framework()
             # terminate_deployment(manifest_docs)
-        except:
-            logging.info("flink-session pod was already down")
+        except Exception as e:
+            logging.error(f"Exception when shutting down {e}")
